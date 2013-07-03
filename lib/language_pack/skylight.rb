1a8b8b7742203a29380705f4a1f9cf5d4ae1e5b7
Terence Lee <hone02@gmail.com>
2013-07-03 15:59:12 -0400

    better display of timing data

--- lib/language_pack/skylight.rb
+++ lib/language_pack/skylight.rb
@@ -2,24 +2,50 @@ require 'benchmark'
 require 'language_pack/shell_helpers'
 
 module Skylight
-  def self.bench_msg(message)
-    Kernel.puts "=> #{message}"
+  def self.bench_msg(message, level = 0)
+    Kernel.puts "#{'==' * level}> #{message}"
     $stdout.flush
   end
 
-  def self.instrument(name, *args)
+  def self.instrument(cat, title = "", *args)
     ret = nil
-    time = Benchmark.realtime { ret = yield }
-    bench_msg "#{name} : #{time}"
+    time = Benchmark.realtime do
+      yield_with_block_depth do
+        ret = yield
+      end
+    end
+    bench_msg("#{cat} : #{time}", block_depth)
 
     return ret
   end
+
   def self.trace(name, *args)
-    ret = nil
-    time = Benchmark.realtime { ret = yield }
-    bench_msg "#{name} : #{time}"
+    ret         = nil
+    block_depth = 0
+
+    time = Benchmark.realtime do
+      yield_with_block_depth do
+        ret = yield
+      end
+    end
+    bench_msg "#{name} : #{time}", block_depth
 
     return ret
+  end
+
+  def self.yield_with_block_depth
+    self.block_depth += 1
+    yield
+  ensure
+    self.block_depth -= 1
+  end
+
+  def self.block_depth
+    Thread.current[:block_depth] || 0
+  end
+
+  def self.block_depth=(value)
+    Thread.current[:block_depth] = value
   end
 end
 
