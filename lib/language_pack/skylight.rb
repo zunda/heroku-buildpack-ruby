60ddd27bd923cf8a4a91d87df9b8b0d8f8cba432
Terence Lee <hone02@gmail.com>
2013-07-03 12:57:01 -0400

    puts time and disable skylight
    Skylight has a 30s timelimit for trace time

--- lib/language_pack/skylight.rb
+++ lib/language_pack/skylight.rb
@@ -1,10 +1,32 @@
-def skylight
-  vendor_folder = File.expand_path("../../../vendor/skylight/gems/", __FILE__)
-  Dir["#{vendor_folder}/*"].each {|dir| $:.unshift "#{dir}/lib" }
-  require 'skylight'
+require 'benchmark'
+require 'language_pack/shell_helpers'
 
-  config = Skylight::Config.load(File.expand_path("../../../config/skylight.yml", __FILE__))
-  Skylight::Instrumenter.start!(config)
+module Skylight
+  extend LanguagePack::ShellHelpers
+
+  def self.instrument(name, *args)
+    ret = nil
+    time = Benchmark.realtime { ret = yield }
+    topic "#{name} : #{time}"
+
+    return ret
+  end
+  def self.trace(name, *args)
+    ret = nil
+    time = Benchmark.realtime { ret = yield }
+    topic "#{name} : #{time}"
+
+    return ret
+  end
+end
+
+def skylight
+#  vendor_folder = File.expand_path("../../../vendor/skylight/gems/", __FILE__)
+#  Dir["#{vendor_folder}/*"].each {|dir| $:.unshift "#{dir}/lib" }
+#  require 'skylight'
+
+#  config = Skylight::Config.load(File.expand_path("../../../config/skylight.yml", __FILE__))
+#  Skylight::Instrumenter.start!(config)
   yield
-  Skylight::Instrumenter.stop!
+#  Skylight::Instrumenter.stop!
 end
