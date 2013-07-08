2167fc6a0d1731dfe624afd19c236dd5cc819d50
Terence Lee <hone02@gmail.com>
2013-07-08 17:07:31 -0400

    add request id and trace should just call instrument

--- lib/language_pack/skylight.rb
+++ lib/language_pack/skylight.rb
@@ -2,35 +2,30 @@ require 'benchmark'
 require 'language_pack/shell_helpers'
 
 module Skylight
-  def self.bench_msg(message, level = 0)
-    Kernel.puts "#{'==' * level}> #{message}"
+  def self.bench_msg(message, level = 0, start_time, end_time, duration, request_id)
+    Kernel.puts "#{'==' * level}> name=#{message}, start=#{start_time}, end=#{start_time}, duration=#{duration}, level=#{level}, request_id=#{request_id}"
     $stdout.flush
   end
 
   def self.instrument(cat, title = "", *args)
-    ret = nil
-    time = Benchmark.realtime do
+    ret        = nil
+    start_time = Time.now.to_f
+    duration = Benchmark.realtime do
       yield_with_block_depth do
         ret = yield
       end
     end
-    bench_msg("#{cat} : #{time}", block_depth)
+    end_time   = Time.now.to_f
+    bench_msg(cat, block_depth, start_time, end_time, duration, request_id)
 
-    return ret
+    ret
   end
 
-  def self.trace(name, *args)
+  def self.trace(name, *args, &blk)
     ret         = nil
     block_depth = 0
 
-    time = Benchmark.realtime do
-      yield_with_block_depth do
-        ret = yield
-      end
-    end
-    bench_msg "#{name} : #{time}", block_depth
-
-    return ret
+    instrument(name) { blk.call }
   end
 
   def self.yield_with_block_depth
@@ -46,6 +41,10 @@ module Skylight
 
   def self.block_depth=(value)
     Thread.current[:block_depth] = value
+  end
+
+  def self.request_id
+    ENV['REQUEST_ID']
   end
 end
 
