bcb286dcf90addb2ace0699bcd956ada22b3b85c
Terence Lee <hone02@gmail.com>
2014-11-14 17:21:20 -0800

    don't support ruby 1.9.2/1.8.7 anymore :)

--- spec/rubies_spec.rb
+++ spec/rubies_spec.rb
@@ -9,31 +9,6 @@ describe "Ruby Versions" do
     end
   end
 
-
-  it "should deploy ruby 1.8.7 properly" do
-    Hatchet::Runner.new("mri_187").deploy do |app|
-      version = '1.8.7'
-      expect(app.output).to match(version)
-      expect(app.run('ruby -v')).to match(version)
-    end
-  end
-
-  it "should deploy ruby 1.9.2 properly" do
-    Hatchet::Runner.new("mri_192").deploy do |app|
-      version = '1.9.2'
-      expect(app.output).to match(version)
-      expect(app.run('ruby -v')).to match(version)
-    end
-  end
-
-  it "should deploy ruby 1.9.2 properly (git)" do
-    Hatchet::GitApp.new("mri_192", buildpack: git_repo).deploy do |app|
-      version = '1.9.2'
-      expect(app.output).to match(version)
-      expect(app.run('ruby -v')).to match(version)
-    end
-  end
-
   it "should deploy ruby 1.9.3 properly" do
     Hatchet::Runner.new("mri_193").deploy do |app|
       version = '1.9.3'
