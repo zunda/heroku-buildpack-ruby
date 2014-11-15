9634eba2137a995f776d250df7f5f85f50be25c6
Terence Lee <hone02@gmail.com>
2014-11-14 18:55:10 -0800

    fix tests that broke when cedar-14 GA'd

--- spec/stack_spec.rb
+++ spec/stack_spec.rb
@@ -2,7 +2,11 @@ require_relative "spec_helper"
 
 describe "Stack Changes" do
   it "should reinstall gems on stack change" do
-    Hatchet::Runner.new("mri_200").deploy do |app, heroku|
+    app = Hatchet::Runner.new("mri_200")
+    app.setup!
+    app.heroku.put_stack(app.name, 'cedar')
+
+    app.deploy do |app, heroku|
       heroku.put_stack(app.name, "cedar-14")
       `git commit --allow-empty -m "cedar-14 migrate"`
 
@@ -14,7 +18,11 @@ describe "Stack Changes" do
   end
 
   it "should not reinstall gems if the stack did not change" do
-    Hatchet::Runner.new("mri_200").deploy do |app, heroku|
+    app = Hatchet::Runner.new("mri_200")
+    app.setup!
+    app.heroku.put_stack(app.name, 'cedar')
+
+    app.deploy do |app, heroku|
       heroku.put_stack(app.name, "cedar")
       `git commit --allow-empty -m "cedar migrate"`
 
--- spec/user_env_compile_edge_case_spec.rb
+++ spec/user_env_compile_edge_case_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
 
 describe "User env compile" do
-  it "should not cause problems with warnings" do
+  it "should not cause problems with warnings", stack: :cedar do
     app = Hatchet::Runner.new("mri_210")
     app.setup!
+    app.heroku.put_stack(app.name, 'cedar')
     app.set_config("RUBY_HEAP_MIN_SLOTS" => "1000000")
     app.deploy do |app|
       expect(app.run("bundle version")).to match(LanguagePack::Ruby::BUNDLER_VERSION)
