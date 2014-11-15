efc3a747995e3ee6d1814ddc21847b2676a47df7
Terence Lee <hone02@gmail.com>
2014-11-14 18:54:47 -0800

    remove more ruby 1.8.7/1.9.2 tests

--- spec/bugs_spec.rb
+++ spec/bugs_spec.rb
@@ -1,15 +1,6 @@
 require_relative 'spec_helper'
 
 describe "Bugs" do
-  context "MRI 1.8.7" do
-    it "should install nokogiri" do
-      Hatchet::Runner.new("mri_187_nokogiri").deploy do |app|
-        expect(app.output).to match("Installing nokogiri")
-        expect(app.output).to match("Your bundle is complete!")
-      end
-    end
-  end
-
   it "nokogiri should use the system libxml2" do
     Hatchet::Runner.new("nokogiri_160").deploy do |app|
       expect(app.output).to match("nokogiri")
